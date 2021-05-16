66c2ce0a4070c9314878632ca0a61b73b5367cd4
naisuu <julius.cathalina@gmail.com>
2021-05-16 12:14:04 +0200

    Add notes for code cleanup

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
         # TODO: Globally load configuration in one spot that can be accessed from all files. this config getting is ugly.
         data_dir = config.get("downloader").get("dest_dir")
         file_manager.extract_faers(data_dir)
-        file_manager.clean_up("data/extracted/ascii")
+        file_manager.clean_up("data/extracted/ascii")  # TODO: Maybe remove all files except for drug files, bypasses filtering.
 
         def contains(filepath: str, *substrings: str) -> bool:
             for substring in substrings:
@@ -51,6 +51,7 @@ if __name__ == '__main__':
         li = []
         for file in tqdm(drug_files):
             df: pd.DataFrame = pd.read_csv(file, sep="$", index_col=None, header=0, low_memory=False)
+            # TODO: move this try except block to its own function to cleanup code.
             try:
                 df["origin"] = file.split("/")[-1].split("\\")[-1]  # Bypass weird windows backslash, not necessary for other OS's
             except Exception as e:
@@ -58,6 +59,11 @@ if __name__ == '__main__':
             li.append(df)
         drug_df: pd.DataFrame = pd.concat(li, axis=0, ignore_index=True)
 
+        if not os.path.exists("data/processed"):
+            try:
+                os.mkdir("data/processed")
+            except PermissionError as e:
+                print(f"Not enough permissions to create file. Caused by: {e}")
         drug_df.to_csv("data/processed/drug_table.csv")
 
     df: pd.DataFrame = pd.read_csv("data/processed/drug_table.csv", low_memory=False)
