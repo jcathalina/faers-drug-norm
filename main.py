49c5563290f723f6036c74066ce4ca3ef5c8d370
naisuu <julius.cathalina@gmail.com>
2021-05-16 12:10:53 +0200

    Finish setup code, needs cleanup.

--- main.py
+++ main.py
@@ -33,26 +33,32 @@ if __name__ == '__main__':
         file_manager.extract_faers(data_dir)
         file_manager.clean_up("data/extracted/ascii")
 
-    def contains(filepath: str, *substrings: str) -> bool:
-        for substring in substrings:
-            if substring in filepath:
-                return True
-        return False
+        def contains(filepath: str, *substrings: str) -> bool:
+            for substring in substrings:
+                if substring in filepath:
+                    return True
+            return False
 
-    # TODO: This can probably be done way nicer, us re.compile to use regex to filter only drug files in glob
-    path = "data/extracted/ascii"
-    all_files = glob.glob(path + "/*.txt")
-    drug_files: List[str] = []
-    for file in all_files:
-        if contains(file, "drug", "DRUG"):
-            drug_files.append(file)
-    del all_files
+        # TODO: This can probably be done way nicer, us re.compile to use regex to filter only drug files in glob
+        path = "data/extracted/ascii"
+        all_files = glob.glob(path + "/*.txt")
+        drug_files: List[str] = []
+        for file in all_files:
+            if contains(file, "drug", "DRUG"):
+                drug_files.append(file)
+        del all_files
 
-    li = []
-    for file in tqdm(drug_files):
-        df: pd.DataFrame = pd.read_csv(file, sep="$", index_col=None, header=0, low_memory=False)
-        df["origin"] = file.split("/")[-1].split("\\")[-1]  # Bypass weird windows backslash, not necessary for other OS's
-        li.append(df)
-    final_df: pd.DataFrame = pd.concat(li, axis=0, ignore_index=True)
+        li = []
+        for file in tqdm(drug_files):
+            df: pd.DataFrame = pd.read_csv(file, sep="$", index_col=None, header=0, low_memory=False)
+            try:
+                df["origin"] = file.split("/")[-1].split("\\")[-1]  # Bypass weird windows backslash, not necessary for other OS's
+            except Exception as e:
+                df["origin"] = file.split("/")[-1]
+            li.append(df)
+        drug_df: pd.DataFrame = pd.concat(li, axis=0, ignore_index=True)
 
-    print(final_df.head())
+        drug_df.to_csv("data/processed/drug_table.csv")
+
+    df: pd.DataFrame = pd.read_csv("data/processed/drug_table.csv", low_memory=False)
+    print(len(df))  # 40_584_418 entries from 2012 Q4 --- 2021 Q1
