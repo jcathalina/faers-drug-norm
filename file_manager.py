094a1b1eb4ac951706d9be0e43084840c624fabb
naisuu <julius.cathalina@gmail.com>
2021-05-16 11:51:31 +0200

    File extraction and loading drug files into dataframes like Banda

--- file_manager.py
+++ file_manager.py
@@ -1,3 +1,46 @@
-class FaersFileManager:
+import os
+import zipfile
+
+from tqdm import tqdm
+
+
+class FileManager:
     def __init__(self):
         pass
+
+    @staticmethod
+    def extract_faers(zip_dir: str):
+        extract_path = os.path.join(zip_dir, "extracted")
+        if not os.path.exists(extract_path):
+            try:
+                os.mkdir(extract_path)
+            except PermissionError as e:
+                print(f"Not enough permissions: {e}. Extracting all zipfiles to pwd")
+                extract_path = zip_dir
+
+        print("Extracting files...")
+        for file in tqdm(os.listdir(zip_dir)):
+            full_filename = os.path.join(zip_dir, file)
+            if zipfile.is_zipfile(full_filename):
+                with zipfile.ZipFile(full_filename) as zf:
+                    zf.extractall(extract_path)
+
+    @staticmethod
+    def clean_up(extracted_dir: str):
+        files_to_remove = []
+        for f in os.listdir(extracted_dir):
+            full_filename = os.path.join(extracted_dir, f)
+            if os.path.isfile(full_filename) and full_filename.endswith((".pdf", ".PDF")):
+                files_to_remove.append(full_filename)
+        print(f"Removing {len(files_to_remove)} PDF files...")
+
+        for file in files_to_remove:
+            try:
+                os.remove(file)
+            except FileNotFoundError as e:
+                print(f"Not able to find {file}, error: {e}")
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,16 +1,58 @@
+from typing import List
+
 import toml
 
 from downloader import ThreadedFileDownloader
 from scraper import Scraper
+from file_manager import FileManager
+from tqdm import tqdm
+
+import pandas as pd
+import glob
+import os
+
 
 if __name__ == '__main__':
-    config = toml.load("config.toml")
-    downloader = ThreadedFileDownloader(configuration=config)
-    scraper = Scraper(configuration=config)
 
-    urls = scraper.run()
+    do_setup = False
 
-    for url in urls:
-        downloader.run(url)
+    if do_setup:
+        # TODO: Extract downloader and file manager to their own scripts, main should only run main loop.
+        config = toml.load("config.toml")
+        downloader = ThreadedFileDownloader(configuration=config)
+        scraper = Scraper(configuration=config)
+        file_manager = FileManager()
 
+        urls = scraper.run()
 
+        for url in urls:
+            downloader.run(url)
+
+        # TODO: Globally load configuration in one spot that can be accessed from all files. this config getting is ugly.
+        data_dir = config.get("downloader").get("dest_dir")
+        file_manager.extract_faers(data_dir)
+        file_manager.clean_up("data/extracted/ascii")
+
+    def contains(filepath: str, *substrings: str) -> bool:
+        for substring in substrings:
+            if substring in filepath:
+                return True
+        return False
+
+    # TODO: This can probably be done way nicer, us re.compile to use regex to filter only drug files in glob
+    path = "data/extracted/ascii"
+    all_files = glob.glob(path + "/*.txt")
+    drug_files: List[str] = []
+    for file in all_files:
+        if contains(file, "drug", "DRUG"):
+            drug_files.append(file)
+    del all_files
+
+    li = []
+    for file in tqdm(drug_files):
+        df: pd.DataFrame = pd.read_csv(file, sep="$", index_col=None, header=0, low_memory=False)
+        df["origin"] = file.split("/")[-1].split("\\")[-1]  # Bypass weird windows backslash, not necessary for other OS's
+        li.append(df)
+    final_df: pd.DataFrame = pd.concat(li, axis=0, ignore_index=True)
+
+    print(final_df.head())
