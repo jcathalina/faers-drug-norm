21949df532ab833bfc567c3f7e4755538deaf2cd
naisuu <julius.cathalina@gmail.com>
2021-06-10 23:33:34 +0200

    Add skeleton of mapping algorithm

--- alg/formatter.py
+++ alg/formatter.py
@@ -3,9 +3,10 @@ Pipeline that takes in a row of faers data from a database and transforms it int
 that is optimized for RxNav's normalization algorithm.
 """
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, Dict
 
 import pandas as pd
+from fuzzywuzzy import process
 
 
 @dataclass
@@ -21,13 +22,14 @@ class FaersDataRow:  # TODO: Think of a better name for this
     def __init__(self, data: pd.Series):
         self.query: str = self._create_query(data=data)
         self.identifier = Identifier(primary_id=data["primaryid"], case_id=data["caseid"], drug_seq=data["drug_seq"])
-        self.nda_num: Optional[str] = data["nda_num"]
+        self.nda_num: "Optional[str]" = data["nda_num"]
         self.role_code: str = data["role_cod"]
-        self.backup_query: Optional[str] = self._create_backup_query(data=data)
+        self.backup_query: "Optional[str]" = self._create_backup_query(data=data)
+        # self.int_mapped_query: "Optional[str]" = None
 
 
     @staticmethod
-    def _create_query(data: pd.Series):
+    def _create_query(data: pd.Series) -> str:
         """
         :param data: a row extracted from a pandas dataframe containing FAERS data
         :return: a query that's optimized (given the available info) for RxNav's approximate matching algorithm
@@ -38,7 +40,7 @@ class FaersDataRow:  # TODO: Think of a better name for this
 
 
     @staticmethod
-    def _create_backup_query(data: pd.Series):
+    def _create_backup_query(data: pd.Series) -> "Optional[str]":
         """
         This method is only a fallback as active ingredient is often missing from FAERS data entries.
         However, sometimes more information is contained in these columns that can be helpful for the mapping
@@ -53,3 +55,42 @@ class FaersDataRow:  # TODO: Think of a better name for this
         components = ["prod_ai", "route", "dose_amt", "dose_unit", "dose_form"]
         query = " ".join([str(data[c]) for c in components])
         return query.replace("nan", "").rstrip()  # remove all nans from string and strip trailing whitespace.
+
+
+    @staticmethod
+    def get_int_mapped_query(data: pd.Series, international_dict: "Dict[str, str]", fuzzy_search=True) -> "Optional[str]":
+        """
+        :return:
+        """
+        drug_name = data["drug_name"]
+        components = ["route", "dose_amt", "dose_unit", "dose_form"]
+
+        if fuzzy_search:
+            choices = [*international_dict]  # unpack dict keys to list for fuzzy matching
+            match = process.extractOne(drug_name, choices=choices, score_cutoff=90)
+            if match is None:
+                return
+            active_ingredient = international_dict.get(match)
+            return f"{active_ingredient} {' '.join([str(data[c]) for c in components])}"
+
+        direct_match = international_dict.get(drug_name, None)
+        return f"{direct_match} {' '.join([str(data[c]) for c in components])}"
+
+    @staticmethod
+    def get_int_mapped_active_ingredient(data: pd.Series, international_dict: "Dict[str, str]", fuzzy_search=True) -> "Optional[str]":
+        # TODO: This can be a heper method for int mapped query to refactor code
+        """
+        :param data:
+        :param international_dict:
+        :param fuzzy_search:
+        :return:
+        """
+        drug_name = data["drug_name"]
+        if fuzzy_search:
+            choices = [*international_dict]  # unpack dict keys to list for fuzzy matching
+            match = process.extractOne(drug_name, choices=choices, score_cutoff=90)
+            if match is None:
+                return
+            active_ingredient = international_dict.get(match)
+            return active_ingredient
+        return international_dict.get(drug_name, None)
--- alg/mapping.py
+++ alg/mapping.py
@@ -1,4 +1,4 @@
-from typing import Dict, Optional, Tuple
+from typing import Dict, Optional, List
 
 import pandas as pd
 import numpy as np
@@ -11,28 +11,60 @@ class RxNormMapper:
     def __init__(self):
         self.thresh = 80
         self.nda_dict = self._load_nda_dict()
+        self.international_dict = self._load_international_dict()  # TODO: Load dictionary that maps int. drug names to active ingredients
+
+        # Analytics
+        self.number_hq_mappings = 0
+        self.successful_name_only_calls = 0
+        self.successful_default_calls = 0
+        self.successful_backup_calls = 0
 
 
-    def map_to_rxnorm(self, data_row: "FaersDataRow"):
+
+    def map_to_rxnorm(self, data_row: "FaersDataRow", include_international=True) -> None:
         """
         Optimizes the data found in FAERS drug tables into formats that are convenient for RxNav's approximate matching
         algorithm to increase the odds of confidently mapping drug entries to RXCUIs.
+        :param include_international:
         :param data_row:
         """
 
         tries_exhausted = False
-        confidence_score = -np.inf
 
-        while confidence_score <= self.thresh and not tries_exhausted:
+        while not tries_exhausted:  # TODO: Make a list of functions and loop through that to make code shorter.
 
-            response: "RxNavResponse" = self.try_mapping_nda_num(data=data_row)
+            response: RxNavResponse = self.try_mapping_nda_num(data=data_row)
             if response.success:
                 print(response)
                 break
 
+            response = self.try_mapping_only_drug_name(data=data_row)
+            if response.success:
+                self.successful_name_only_calls += 1
+                print(response)
+                break
 
+            response = self.try_mapping_default_query(data=data_row)
+            if response.success:
+                self.successful_default_calls += 1
+                print(response)
+                break
 
+            response = self.try_mapping_backup_query(data=data_row)
+            if response.success:
+                self.successful_backup_calls += 1
+                print(response)
+                break
 
+            if include_international:
+                response = self.try_mapping_with_international_data(data=data_row)
+                # TODO: The international mapper has to fuzzy match against the EU dict to find the closest match or direct matching?
+                if response.success:
+                    print(response)
+                    break
+
+            print("No high-confidence mapping found... ")
+            tries_exhausted = True
 
         # if we receive a low confidence score
         # if res.candidates[0].score <= self.thresh:
@@ -40,12 +72,13 @@ class RxNormMapper:
 
     def try_mapping_nda_num(self, data) -> "RxNavResponse":
         if data.nda_num:  # if an nda_num is present, check it against the dictionary first.
-            nda_mapping: "Optional[str]" = self.nda_dict.get(data.nda_num, None)
-            if nda_mapping is None:
-                pass
-            return approx_match(query=nda_mapping)
+            trade_name: "Optional[str]" = self.nda_dict.get(data.nda_num, None)
+            if trade_name is None:
+                return RxNavResponse("", [])  # Just return an empty default RxNavResponse to not waste time on this process.
+            return approx_match(query=trade_name)
 
     @staticmethod
     def _load_nda_dict(path: str) -> Dict:
+        print("Loading NDA dictionary...")
         nda_df = pd.read_csv(path, header=None)
         return {nda_df["nda_num"]: nda_df["trade_name"]}
